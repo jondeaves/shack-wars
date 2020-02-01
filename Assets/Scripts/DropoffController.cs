09fbd12b6885b89f541ceb95681715cc0d2f2956
Jon Deaves <hello@jondeaves.me>
2020-02-01 13:40:02 +0000

    chore: map layout tweaks

--- Assets/Scripts/DropoffController.cs
+++ Assets/Scripts/DropoffController.cs
@@ -32,7 +32,7 @@ public class DropoffController : MonoBehaviour
             // Score point to player
             collision.gameObject.GetComponent<PlayerController>().AddScore(1);
 
-            playerController.CurrentPickup.transform.position = new Vector3(5f, 0.45f, 5f);
+            playerController.CurrentPickup.transform.position = new Vector3(6f, 0.45f, 6f);
             playerController.CurrentPickup.GetComponent<PickupController>().SetTarget(null);
 
         }
